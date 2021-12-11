74748533869b261196ff8de98311526217b27267
Daniel Namir <ddnnll@email.arizona.edu>
2021-12-10 21:23:27 -0700

    update

--- findnearbyplaces/components/layout.js
+++ findnearbyplaces/components/layout.js
@@ -1,34 +1,28 @@
 import { Navbar, NavDropdown, Nav, Container } from "react-bootstrap";
 const Layout = () => {
-  return (
-    <>
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
-  );
+    return (
+        <>
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action 1</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Action 2</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Action 3</NavDropdown.Item>
+                            <NavDropdown.Divider/>
+                            <NavDropdown.Item href="#action/3.4">Action 4</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        </>
+    );
 };
 
 export default Layout;
--- findnearbyplaces/src/App.js
+++ findnearbyplaces/src/App.js
@@ -1,22 +1,17 @@
 import "./App.css";
 import Login from "./components/login";
 import Places from "./components/places";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Container } from "react-bootstrap";
-import Layout from "./components/layout";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Container>
-        <Layout />
-        <Routes>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="place/:place" element={<Places />}></Route>
-          <Route path="/home" element={<h1>Home Page</h1>}></Route>
-        </Routes>
-      </Container>
-    </BrowserRouter>
+    <HashRouter>
+      <Switch>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="place/:PLACE" component={Places}/>
+        <Route exact path="/home" element={<h1>Home Page</h1>}></Route>
+      </Switch>
+    </HashRouter>
   );
 }
 
